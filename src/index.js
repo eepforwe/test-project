6bbecfecc4b062f03aff719d6c025a35b0eb48cb
iskaldvind <iskaldvind@gmail.com>
2017-07-10 14:28:56 +0300

    6

--- src/index.js
+++ src/index.js
@@ -6,24 +6,26 @@ import rollbar from 'rollbar';
 import path from 'path';
 import getWebpackConfig from '../webpack.config.babel';
 
-const app = new Koa();
-const pug = new Pug({
-  viewPath: path.join(__dirname, 'views'),
-  baseDir: path.join(__dirname, 'views'),
-});
-app.use(pug);
 
+const app = new Koa();
 const router = new Router();
-app.use(router.allowedMethods());
-app.use(router.routes());
 
 router.get('/', (ctx) => {
   ctx.render('welcome');
 });
 
-const rollbarTocken = process.env.ROLLBAR_TOCKEN;
-rollbar.init(rollbarTocken);
-app.use(rollbar.errorHandler(rollbarTocken));
+app.use(router.allowedMethods());
+app.use(router.routes());
+
+const pug = new Pug({
+  viewPath: path.join(__dirname, 'views'),
+  baseDir: path.join(__dirname, 'views'),
+});
+pug.use(app);
+
+//const rollbarTocken = process.env.ROLLBAR_TOCKEN;
+//rollbar.init(rollbarTocken);
+//app.use(rollbar.errorHandler(rollbarTocken));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
